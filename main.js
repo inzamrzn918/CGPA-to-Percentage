b04bf9fa3bf04e2d0532aa637b1efe9e8f49e40f
Inzamul Hoque <inzamol@gmail.com>
2022-01-03 19:33:56 +0530

    Dataset added

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 $(document).ready(()=>{
     var cgpaField = $('#cgpa');
-    var university_formula = 10
+
+    var unv = $('#unv');
+    var unvHtml = `<option value=""> Select University</option>`;
+    $.getJSON('universities.json', (jd)=>{
+        console.log(jd)
+        for(var i=0; i<jd.length; i++){
+            unvHtml += 
+            `<option value="${jd[i].calc}">${jd[i].name}</option>
+            `
+        }
+        unv.html(unvHtml)
+    })
     
 
     cgpaField.on('keyup',()=>{
         var value = cgpaField.val()
+        var unv_fld = $('#unv');
         console.log(value)
         if(!isNaN(value)){
             if(value<=10){
-                var perc = value*university_formula;
+                var perc = value*(unv.val());
                 $('#result').html(
                     `<h3 class="alert alert-success" >Percentage : ${perc} %</h3>`
                 )
